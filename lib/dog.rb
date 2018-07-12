45f976fce10a75e2254c513a0680982c22a4a839
marissa-a <marissa.andrade@verizon.net>
2018-07-12 22:00:52 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,13 @@
 # dog.rb
 class Dog(bark,sit)
-  def initialize 
-    @bark=bark
-    @sit=sit
-  end
   
   def bark
-    @bark
+    "Woof!"
   end
   
   def sit
-    @sit
+    "The dog is sitting"
   end
-  
-  dog_one=dog.new"Woof!", "The Dog is sitting"
   
   puts Dog
   
