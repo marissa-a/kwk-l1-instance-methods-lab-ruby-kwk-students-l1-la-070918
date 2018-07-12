3b47b4304afb4e78a9aec753459e4548a63ac8f2
marissa-a <marissa.andrade@verizon.net>
2018-07-12 21:53:07 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,19 @@
 # dog.rb
-class Dog
-  def initialize
-    @bark="Woof"
+class Dog(bark,sit)
+  def initialize 
+    @bark=bark
+    @sit=sit
   end
+  
+  def bark
+    @bark
+  end
+  
+  def sit
+    @sit
+  end
+  
+  dog_one="Woof!", "The Dog is sitting"
+  
   puts Dog
+  
